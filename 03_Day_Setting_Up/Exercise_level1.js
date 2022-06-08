0b36f0e8fbf5ce59c2a81cc7f1b1f81fb63fcde9
scralexander <scralexandercr@gmail.com>
2022-06-09 01:34:36 +0800

    Re-Cap CSS and try making userCard

---
+++ 03_Day_Setting_Up/Exercise_level1.js
@@ -0,0 +1,23 @@
+/* 
+Module is exported block code that can be used.
+
+Package is bundle of module.
+
+Module just block of code that responsible one thing only, package in otherhand have multiple responsible it like an application.
+
+NPM is program to manage package easier.
+
+Webpack -> bundleing tool, so all file that makes up the website merge in one file (like rar / zip)
+
+Easier using boilerplate using command npx create-react-app <name-app>
+
+package.json file that control version of package, package-lock/yarn.lock create when we already install package one so it cant be change, .gitignore list of file/folder that we dont want add to git, node_modules all package that has been downloaded, public folder inside have root html file and otherthing that root html need.
+
+Still learning vscode but overall vscode is enough.
+
+Prettier make formating code easier and pretty, ESLint helping indentified what missing or not standard code, snippet also help for write boilerplate code.
+
+We use export <module> -> and import with {}
+we use export default <module> -> and import without {} just variable name;
+
+*/
